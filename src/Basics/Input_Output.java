550becc49906460b984f725582dbed1db7f4a092
Priyanshu-ps14 <priyanshusingh140403@gmail.com>
2026-02-04 22:07:48 +0530

    Day 1: Basics

--- src/Basics/Input_Output.java
+++ src/Basics/Input_Output.java
@@ -1,4 +1,17 @@
 package Basics;
+import java.util.Scanner;
 
 public class Input_Output {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + "! Welcome to Java programming.");
+        System.out.print("Enter your age: ");
+        int Age = sc.nextInt();
+        System.out.println("You are " + Age + " years old.");
+
+    }
+
 }
