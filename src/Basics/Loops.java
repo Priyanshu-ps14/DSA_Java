6861ced65f091cbe957bceffffa511d33fa9c386
Priyanshu-ps14 <priyanshusingh140403@gmail.com>
2026-02-04 22:37:32 +0530

    Day 1: Basics

--- src/Basics/Loops.java
+++ src/Basics/Loops.java
@@ -1,24 +1,26 @@
 package Basics;
 
+import java.util.Scanner;
 
 public class Loops {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
         System.out.println();
 
-        for (int i = 2; i<=20; i+=2){
-            System.out.print(i+" ");
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.print(i + " ");
         }
         System.out.println();
 
-        for (int i = 2;i<=10; i++){
-            for (int j=1;j<=10;j++){
-                System.out.println(i+"x"+j+"="+(i*j));
-            }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to print its multiplication table: ");
+        int num = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
         }
-         System.out.println();
     }
+
 }
