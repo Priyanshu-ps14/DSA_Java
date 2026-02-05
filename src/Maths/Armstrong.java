d70740ab01b8d8eb5f802cf1fb535a9237cff242
Priyanshu-ps14 <priyanshusingh140403@gmail.com>
2026-02-06 00:18:43 +0530

    Day 2: Maths

--- src/Maths/Armstrong.java
+++ src/Maths/Armstrong.java
@@ -1,31 +1,29 @@
 package Maths;
-import java.util.Scanner;
+
 
 public class Armstrong {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number : ");
-        int num = sc.nextInt();
+
+        for (int i = 100; i <= 1000; i++) {
+            if (isArmstrong(i)){
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    static boolean isArmstrong(int num) {
+
         int original_num = num;
         int sum = 0;
         int rem;
 
 
-        while(num != 0) {
-            rem = num%10;
+        while (num != 0) {
+            rem = num % 10;
             num /= 10;
-            sum = sum + rem*rem*rem;
-
+            sum = sum + rem * rem * rem;
         }
-        if (sum == original_num) {
-            System.out.println("The number is armstrong.");
-        }
-        else {
-            System.out.println("The number is not armstrong.");
-        }
-
+        return sum == original_num;
     }
-
-
 }
